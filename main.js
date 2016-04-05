b156a0dd44cef1535c9c5f0a0072c7a02de1bd3d
Shohei Miyashita <shohei.miyashita@kamasu.jp>
2016-04-05 14:59:38 +0900

    fix up

--- main.js
+++ main.js
@@ -3,57 +3,90 @@ var fs      = require('fs');
 
 config = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
 
-
 if (!fs.existsSync('dist')) {
   fs.mkdirSync('dist');
 }
 
-var requests = config.map(function (element) {
-  return function () {
-    var dir = 'dist/' + element.class;
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-
-    element.keywords.forEach(function (keyword) {
-
-      var options = {
-        url: 'https://api.photozou.jp/rest/search_public.json',
-        qs: { keyword: keyword },
-        method: 'GET',
-        json: true
-      }
-      request(options, function (error, response, body) {
-        if (body.stat === 'ok') {
-          body.info.photo.forEach(function (photo) {
-            request({
-              url: photo.image_url,
-              method: 'GET',
-              json: false,
-              encoding: 'binary'
-            }, function (error, response, data) {
-              fs.writeFile(dir + "/" + photo.photo_id + ".jpg", data , 'binary', function (err) {
-                if (err) { console.log(err); }
-              });
-            });
-          });
-        }
-      });
-
-    });
-  };
-
-});
-
-function main() {
-  if (requests.length == 0) {
-    console.log('finish!');
-    return;
-  }
-  console.log('request...');
-  requests.pop()();
-  console.log('waiting for API limitation...');
-  setTimeout(main, 2000);
+function successDot() {
+  process.stdout.write('\033[32m.\033[39m');
 }
 
-main();
+function errorDot() {
+  process.stdout.write('\033[31m.\033[0m');
+}
+
+// send request API according to config.json.
+// And, gather them out into a Array
+var count = 0;
+var promises = config.map(function (element) {
+  var dir = 'dist/' + element.class;
+  if (!fs.existsSync(dir)) { fs.mkdirSync(dir); }
+  return element.keywords.map(function (keyword) {
+    return new Promise(function (result, reject) {
+      setTimeout(function () {
+        request({
+          url: 'https://api.photozou.jp/rest/search_public.json',
+          qs: { keyword: keyword },
+          method: 'GET',
+          json: true
+        }, function (error, response, body) {
+          if (error) {
+            result([]);
+          } else if (body.stat === 'ok') {
+            console.log('Search with keyword `' + keyword + '`');
+            console.log(element.class + ' count: ' + body.info.photo.length);
+            body.info.photo.forEach(function (p) { p.dir = element.class; });
+            result(body.info.photo);
+          } else {
+            result([]);
+          }
+        });
+      }, count * 2000);
+      count += 1;
+    });
+  });
+});
+
+console.log('Requesting API');
+Promise.all(promises.reduce(function (next, acc) { return acc.concat(next); }, []))
+  .then(function (requests) {
+    process.stdout.write('\nProcessing...');
+    console.log()
+    return Promise.resolve(requests
+      .reduce(function (next, acc) { return acc.concat(next); }, [])
+      .filter(function (photo) { return photo.image_url; } ));
+  })
+  .then(function (photos) {
+    process.stdout.write('\nGathering ' + photos.length + ' images from WEB\n');
+    return Promise.all(photos.map(function (photo) {
+      return new Promise(function (result, reject) {
+        request({
+          url: photo.image_url,
+          method: 'GET',
+          json: false,
+          encoding: 'binary'
+        }, function (error, response, data) {
+          if (error) { errorDot(); reject(); }
+          successDot();
+          result({info: photo, data: data});
+        });
+      });
+    }));
+  })
+  .then(function (photos) {
+    process.stdout.write('\nWriting image onto local\n');
+    return Promise.all(photos.map(function (photo) {
+      return new Promise(function (result, reject) {
+        fs.writeFile("dist/" + photo.info.dir + "/" + photo.info.dir + "_" + photo.info.photo_id + ".jpg", photo.data, 'binary', function (err) {
+          if (err) { errorDot(); return; }
+          successDot();
+          result();
+        });
+      });
+    }));
+  })
+  .then(function (_) {
+    console.log('\nFinish!');
+  })
+  .catch(function (error) { console.log(error.stack); });
+
